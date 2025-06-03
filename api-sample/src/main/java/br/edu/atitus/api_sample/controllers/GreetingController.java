a9957f075e5a2ff3cfdaaf18825702400bb910ed
PEDRO HENRIQUE SCHAEDLER <1137073@ATITUS.LOCAL>
2025-06-02 22:12:33 -0300

    Minha primeira aplicação Spring Boot

---
+++ api-sample/src/main/java/br/edu/atitus/api_sample/controllers/GreetingController.java
@@ -0,0 +1,17 @@
+package br.edu.atitus.api_sample.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greenting")
+public class GreetingController {
+
+	
+	@GetMapping
+	public String getGreeting() {
+		return "Hello word";
+	}
+	
+}
